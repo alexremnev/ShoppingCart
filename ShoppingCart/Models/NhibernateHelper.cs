808e543a1d9d62ab1e779f7d307d7083157c9e83
alexr <alexr@DESKTOP-QAKCS28>
2016-09-05 08:27:25 -0700

    NHibernateHelper

---
+++ ShoppingCart/Models/NhibernateHelper.cs
@@ -0,0 +1,37 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+using ShoppingCart.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+
+namespace ShoppingCart.Models
+{
+    public class NhibernateHelper
+    {
+        private static ISessionFactory _sessionfactory;
+
+        private static ISessionFactory Sessionfactory
+        {
+            get
+            {
+                if (_sessionfactory == null)
+                {
+                    var configuration = new Configuration();
+                    configuration.Configure();
+                    configuration.AddAssembly(typeof(Products).Assembly);
+                    _sessionfactory = configuration.BuildSessionFactory();
+                    
+                }
+                return _sessionfactory;
+            }
+        }
+        public static ISession OpenSession()
+        {
+            return Sessionfactory.OpenSession();
+        }
+
+    }
+}
